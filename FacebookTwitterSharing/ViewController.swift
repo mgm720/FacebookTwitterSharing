e4132fa31de3afc87ab1c43a9867842bb368406d
Michael Miles <michael.gage.miles@gmail.com>
2019-07-06 15:58:36 -0700

    Finished project.

--- FacebookTwitterSharing/ViewController.swift
+++ FacebookTwitterSharing/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var maverickImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func twitterButtonPressed(_ sender: Any) {
+        if let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter) {
+            vc.setInitialText("Check out my dawg http://www.google.com/")
+            vc.add(maverickImageView.image)
+            present(vc, animated: true)
+        }
+    }
+    
+    @IBAction func facebookButtonPressed(_ sender: Any) {
+        if let vc = SLComposeViewController(forServiceType: SLServiceTypeFacebook) {
+            vc.setInitialText("Maverick is Facebook friendly")
+            vc.add(maverickImageView.image)
+            present(vc, animated: true)
+        }
+    }
+    
 }
 
